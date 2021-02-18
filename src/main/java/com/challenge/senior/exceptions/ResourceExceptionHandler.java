49ab14406c677ba091da2d9165c71ca3263a7ef0
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-02-18 07:36:14 -0300

    fix: handle PropertyValueException

--- src/main/java/com/challenge/senior/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/challenge/senior/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.challenge.senior.exceptions;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.hibernate.PropertyValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -56,13 +58,28 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(InvalidFormatException.class)
-    public ResponseEntity<StandardError> methodArgumentNotValidException(final InvalidFormatException exception,
-                                                                         final HttpServletRequest request) {
+    public ResponseEntity<StandardError> invalidFormatException(final InvalidFormatException exception,
+                                                                final HttpServletRequest request) {
         final StandardError error = new StandardError(
                 Instant.now(),
                 BAD_REQUEST,
                 "Invalid format",
                 "Some JSON attributes were sent with the wrong format.",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<StandardError> propertyValueException(
+            final PropertyValueException exception,
+            final HttpServletRequest request
+    ) {
+        final StandardError error = new StandardError(
+                Instant.now(),
+                BAD_REQUEST,
+                "Bad request",
+                "Some required JSON attributes were not sent or references an unsaved transient instance.",
                 request.getRequestURI()
         );
         return ResponseEntity.status(BAD_REQUEST).body(error);
